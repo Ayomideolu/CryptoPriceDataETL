35c073b013abc438b0cf688a384d07ecf647688b
root <root@AYOMIDE>
2023-11-17 11:36:53 +0100

    added a function to write into s3 bucket

--- etl.py
+++ etl.py
@@ -52,3 +52,11 @@ def read_multi_files_from_s3(bucket_name, prefix):
     dfs = [pd.read_csv(io.BytesIO(obj['Body'].read())) for obj in objs]
     data = pd.concat(dfs)
     return data
+
+def write_to_s3(data, bucket_name, folder):
+    file_name = f"crypto_price_data_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv" # Create a file name
+    csv_buffer = StringIO() # Create a string buffer to collect csv string
+    data.to_csv(csv_buffer, index=False) # Convert dataframe to CSV file and add to buffer
+    csv_str = csv_buffer.getvalue() # Get the csv string
+    # using the put_object(write) operation to write the data into s3
+    s3_client.put_object(Bucket=bucket_name, Key=f'{folder}/{file_name}', Body=csv_str ) 
